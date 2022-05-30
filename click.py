b3d6f1555001d49b023887a4347d77eb71e3ec96
paszczep <potfurzbagien@gmail.com>
2022-05-30 21:54:31 +0200

    get product info and click through pages

--- click.py
+++ click.py
@@ -4,41 +4,99 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.common.exceptions import JavascriptException, NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException, JavascriptException, NoSuchElementException, StaleElementReferenceException
+# from selenium.webdriver.chrome.options import Options
+# chrome_options = Options()
+# chrome_options.add_experimental_option("detach", True)
+
+from get_product_info import get_product_info
 
 import time
 import random
+import json
 from bs4 import BeautifulSoup, NavigableString, Tag
 
 PROCESS_MAP = {
-    'Dishwashers': ['LG', 'Samsung'],
-    'Refrigerators': ['Whirlpool', 'GE Appliances'],
-    'Mattresses': ['Sealy']
+    'Dishwashers': [
+        # 'LG',
+        # 'Samsung'
+    ],
+    'Refrigerators': [
+        # 'Whirlpool',
+        # 'GE Appliances'
+    ],
+    'Mattresses': [
+        'Sealy'
+    ]
 }
 
-CHROME_WEBDRIVER_PATH = 'chromedriver.exe'
+
+def get_web_driver():
+    CHROME_WEBDRIVER_PATH = 'chromedriver.exe'
+    used_webdriver = webdriver.Chrome(CHROME_WEBDRIVER_PATH)
+    return used_webdriver
 
 
 def pretend_to_be_human(driver):
-    rand_int_seconds = random.randint(3, 5)
+    rand_int_seconds = random.randint(5, 7)
     rand_float_seconds = random.random()
+    time.sleep(float(rand_int_seconds) - rand_float_seconds*3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
     try:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         for push_that_button in range(rand_int_seconds):
             body = driver.find_element_by_css_selector('body')
             body.send_keys(Keys.PAGE_UP)
-            driver.implicitly_wait(rand_float_seconds/2)
-    except StaleElementReferenceException:
+            time.sleep(rand_float_seconds/3)
+            # driver.implicitly_wait(rand_float_seconds/2)
+    except StaleElementReferenceException as ex:
+        print(ex)
         pretend_to_be_human(driver)
 
 
-def run_the_damn_program():
-    driver = webdriver.Chrome(CHROME_WEBDRIVER_PATH)
+def get_products_from_page(page_driver):
+    driver = page_driver
+    results_css_select = "div.results-wrapped"
+    results_field = WebDriverWait(driver, 10).until(
+        ec.presence_of_element_located((By.CSS_SELECTOR, results_css_select)))
+
+    product_tiles = results_field.find_elements_by_css_selector("div.desktop.product-pod")
+
+    print(f'found {len(product_tiles)} products')
+    page_products = []
+    for product_tile in product_tiles:
+        product_field = product_tile.get_attribute('outerHTML')
+        product_soup = BeautifulSoup(product_field, 'html.parser')
+        product_info_dict = get_product_info(product_soup)
+        page_products.append(product_info_dict)
+
+    return page_products
+
+
+def go_through_pages(driver):
+
+    return None
+
+
+def run_again_after_exception(program):
+    try:
+        program()
+    except StaleElementReferenceException as ex:
+        print('StaleElementReferenceException', ex)
+        run_again_after_exception(program())
+    except TimeoutException as timex:
+        print('TimeoutException', timex)
+        run_again_after_exception(program())
+
+
+def get_shop_products(used_driver, shop_url):
+    driver = used_driver
 
     store_location_urls = ['https://www.homedepot.com/l/Lemmon-Ave/TX/Dallas/75209/589',
                            'https://www.homedepot.com/l/Manhattan-59th-Street/NY/New-York/10022/6177']
-
-    driver.get(store_location_urls[1])
+    shop_url = store_location_urls[1]
+    driver.get(shop_url)
 
     button_xpath = "//*[contains(text(), 'Shop This Store')]"
     select_store_button = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, button_xpath)))
@@ -47,74 +105,66 @@ def run_the_damn_program():
     time.sleep(3)
 
     for sub_department, brands in PROCESS_MAP.items():
-        for product_brand in brands:
+        if len(brands) > 0:
+            for product_brand in brands:
 
-            driver.get('https://www.homedepot.com/c/site_map')
-            # time.sleep(3)
-            sub_department_link = driver.find_element_by_link_text(sub_department)
-            # sub_department_link = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.LINK_TEXT, sub_department)))
-            webdriver.ActionChains(driver).move_to_element(sub_department_link).click(sub_department_link).perform()
+                driver.get('https://www.homedepot.com/c/site_map')
 
-            if sub_department == 'Mattresses':
-                brand_search = driver.find_element_by_css_selector('input.dimension__filter')
-                brand_search.send_keys(product_brand)
-                brand_xpath = f"//*[contains(text(), '{product_brand}')]"
-                select_brand_button = driver.find_element_by_xpath(brand_xpath)
-                select_brand_button.click()
-            else:
-                driver.find_element_by_link_text(product_brand).click()
+                time.sleep(3)
+                # driver.implicitly_wait(3)
+                sub_department_link = driver.find_element_by_link_text(sub_department)
+                # sub_department_link = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.LINK_TEXT, sub_department)))
+                webdriver.ActionChains(driver).move_to_element(sub_department_link).click(sub_department_link).perform()
 
-            pretend_to_be_human(driver)
+                if sub_department == 'Mattresses':
+                    brand_search = WebDriverWait(driver, 10).until(
+                        ec.presence_of_element_located((By.CSS_SELECTOR, 'input.dimension__filter')))
+                    brand_search.send_keys(product_brand)
+                    brand_xpath = f"//*[contains(text(), '{product_brand}')]"
+                    time.sleep(3)
+                    select_brand_button = WebDriverWait(driver, 10).until(
+                        ec.presence_of_element_located((By.XPATH, brand_xpath)))
+                    select_brand_button.click()
+                    driver.refresh()
+                else:
+                    WebDriverWait(driver, 10).until(
+                        ec.presence_of_element_located((By.LINK_TEXT, product_brand))).click()
 
-            soup = BeautifulSoup(driver.page_source, 'lxml')
+                all_products = []
 
-            product_tiles = soup.find_all("div", {"class": "desktop product-pod"})
-            product_dict = {}
-            for tile in product_tiles:
-                # print('begin tile')
-                # print(tile)
-                product_url = tile.find("a", {"class": "header product-pod--ie-fix"})['href']
-                print('url', f'https://www.homedepot.com{product_url}')
-                product_brand = tile.find('span', {"class": "product-pod__title__brand--bold"})
-                if product_brand is not None:
-                    print('brand', product_brand.text)
-                print('title', tile.find('span', {"class": "product-pod__title__product"}).text)
-                product_model = tile.find('div', {"class": "product-identifier product-identifier__model"})
-                if product_model is not None:
-                    print('model', product_model.text)
-                main_price_div = tile.find('div', {"class": "price-format__main-price"})
-                if main_price_div is not None:
-                    main_price = [el.text for el in main_price_div.find_all('span')]
-                    print('main_price', f'{main_price[0]}{main_price[1]}.{main_price[2]}')
-                if 'See Lower Price in Cart' in str(tile):
-                    lower_price_in_cart = True
-                    print('manufacturer price', lower_price_in_cart)
-                tile_was_price = tile.find('div', {"class": "price__was-price"})
-                if tile_was_price is not None:
-                    was_price = [el.text for el in tile_was_price.find_all('span') if '/' not in el.text]
-                    if len(was_price) > 0:
-                        print('was_price', was_price[0])
-                # print('price wrapper', tile.find('div', {"class": 'price__wrapper'}))
-                prod_specs_div = tile.find('div', {"class": "kpf__specs kpf__specs--simple kpf__specs--one-column"})
+                pretend_to_be_human(driver)
+                current_page = 0
+                next_page = 1
+                while current_page < next_page:
+                    page_products = get_products_from_page(page_driver=driver)
+                    all_products.append(page_products)
+                    pagination_items = driver.find_elements_by_css_selector("li.hd-pagination__item")
+                    text_items = [item.text for item in pagination_items if item != '']
+                    if text_items:
+                        next_page += 1
+                        pagination_area = driver.find_element_by_css_selector("nav.hd-pagination")
+                        try:
+                            next_page_button = pagination_area.find_element_by_link_text(str(next_page))
+                            webdriver.ActionChains(driver).move_to_element(next_page_button).click(
+                                next_page_button).perform()
+                            time.sleep(5)
+                            current_page += 1
+                        except NoSuchElementException:
+                            next_page -= 2
+                    else:
+                        next_page = 0
+                return all_products
 
-                if prod_specs_div is not None:
-                    prod_specs_divs = prod_specs_div.find_all('div')
-                    print(prod_specs_divs)
-                    property_names = [el.text for el in prod_specs_div.find_all('div') if 'kpf__name' in str(el['class'])]
-                    property_values = [el.text for el in prod_specs_div.find_all('div') if 'kpf__value' in str(el['class'])]
-                    print('properties', prod_specs_div)
-                    prod_prop_dict = dict(zip(property_names, property_values))
-                    print(prod_prop_dict)
-                print('end tile')
-                print()
 
-            pagination_items = driver.find_elements_by_css_selector("li.hd-pagination__item")
-            text_items = [item.text for item in pagination_items if item != '']
-            if text_items:
-                displayed_page = int(text_items[0])
-                go_to_page = displayed_page + 1
-                print('goto page', go_to_page)
+def run_through_shops(driver, shop_url_list):
+
+    all_shops
+
+    for url in shop_url_list:
+        products = get_shop_products(driver, url)
 
 
 if __name__ == '__main__':
-    run_the_damn_program()
+    driver = get_web_driver()
+    get_shop_products(used_driver=driver)
+    # run_again_after_exception()
--- get_product_info.py
+++ get_product_info.py
@@ -1,52 +1,72 @@
 import json
 
 
-def get_product_info(tile):
+def price_string_from_div(price_div):
+    price_els = [el.text for el in price_div.find_all('span')]
+    price_str = f'{price_els[0]}{price_els[1]}.{price_els[2]}'
+    return price_str
+
+
+def get_product_info(product_soup):
     product_dict = {}
 
-    product_url = tile.find("a", {"class": "header product-pod--ie-fix"})['href']
+    product_url = product_soup.find("a", {"class": "header product-pod--ie-fix"})['href']
     product_dict['url'] = f'https://www.homedepot.com{product_url}'
 
-    product_brand = tile.find('span', {"class": "product-pod__title__brand--bold"})
+    product_brand = product_soup.find('span', {"class": "product-pod__title__brand--bold"})
     if product_brand is not None:
         product_dict['brand'] = product_brand.text
 
-    product_dict['title'] = tile.find('span', {"class": "product-pod__title__product"}).text
+    product_dict['title'] = product_soup.find('span', {"class": "product-pod__title__product"}).text
 
-    product_model = tile.find('div', {"class": "product-identifier product-identifier__model"})
+    product_model = product_soup.find('div', {"class": "product-identifier product-identifier__model"})
     if product_model is not None:
         product_dict['model'] = product_model.text
 
-    main_price_divs = tile.find_all('div', {"class": "price-format__main-price"})
-    print(main_price_divs)
-    if main_price_divs is not None:
-        if len(main_price_divs) == 1:
-            main_price = [el.text for el in main_price_divs[0].find_all('span')]
-            product_dict['main_price'] = f'{main_price[0]}{main_price[1]}.{main_price[2]}'
-        else:
-            print('in here you', main_price_divs)
+    # main_price_divs = tile.find_all('div', {"class": "price"})
+    price_divs = product_soup.find_all('div', {"class": "price"})
+    if len(price_divs) == 2:
+        price_lower, price_upper,  = price_divs[0], price_divs[1]
+        price_lower = price_string_from_div(price_lower)
+        price_upper = price_string_from_div(price_upper)
+        product_dict['main_price'] = f'{price_lower} - {price_upper}'
+        # product_dict['range_price'] = True
+    elif len(price_divs) == 1:
+        product_dict['main_price'] = price_string_from_div(price_divs.pop())
+        # product_dict['range_price'] = False
 
-    lower_price_element = tile.find('div', {"class": "price-detailed__lower-price-wrapper"})
-    if lower_price_element is not None:
-        if lower_price_element.text == 'See Lower Price in Cart':
-            product_dict['manufacturer price'] = True
+        # for price_div in price_divs:
+        #     print('price_div', price_div)
+        #     if price_div is not None:
+        #         # print('price div', price_div)
+        #         main_price_els = price_div.find('div', {'class': "price-format__main-price"})
+        #         price_els = [el.text for el in main_price_els.find_all('span')]
+        #         print(f'{price_els[0]}{price_els[1]}.{price_els[2]}')
+                # for price_el in main_price_els:
+                #     print('price_el', price_el)
 
-    tile_was_price = tile.find('div', {"class": "price__was-price"})
+    # lower_price_element = tile.find('div', {"class": "price-detailed__lower-price-wrapper"})
+    # if lower_price_element is not None:
+    #     if lower_price_element.text == 'See Lower Price in Cart':
+    #         product_dict['manufacturer price'] = True
+
+    tile_was_price = product_soup.find('div', {"class": "price__was-price"})
     if tile_was_price is not None:
         was_price = [el.text for el in tile_was_price.find_all('span') if '/' not in el.text]
         if len(was_price) > 0:
             product_dict['was_price'] = was_price[0]
+    else:
+        was_price = product_soup.find('span', {'class': "u__strike"})
+        if was_price is not None:
+            product_dict['was_price'] = was_price.text
 
-    tile_price = tile.find('div', {'class': 'price'})
-    print(tile_price)
-
-    prod_specs_div = tile.find('div', {"class": "kpf__specs kpf__specs--simple kpf__specs--one-column"})
+    prod_specs_div = product_soup.find('div', {"class": "kpf__specs kpf__specs--simple kpf__specs--one-column"})
     if prod_specs_div is not None:
         property_names = [el.text for el in prod_specs_div.find_all('div') if 'kpf__name' in str(el['class'])]
         property_values = [el.text for el in prod_specs_div.find_all('div') if 'kpf__value' in str(el['class'])]
         prod_prop_dict = dict(zip(property_names, property_values))
         product_dict['specs'] = json.dumps(prod_prop_dict)
-
         product_dict['specs'] = prod_prop_dict
-
+    for key, value in product_dict.items():
+        print(key, value)
     return product_dict
